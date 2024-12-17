f9ca7dfbc8eeb842c8ef2adca53578dd739ea950
xulihang <xulihanghai@163.com>
2024-12-17 14:25:38 +0800

    display the scanning result

--- app/src/main/java/com/tonyxlh/vinscanner/MainActivity.java
+++ app/src/main/java/com/tonyxlh/vinscanner/MainActivity.java
@@ -3,22 +3,31 @@ package com.tonyxlh.vinscanner;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.dynamsoft.core.basic_structures.CapturedResultItem;
 import com.dynamsoft.core.basic_structures.CompletionListener;
 import com.dynamsoft.core.basic_structures.DSRect;
+import com.dynamsoft.core.basic_structures.EnumCapturedResultItemType;
 import com.dynamsoft.cvr.CaptureVisionRouter;
 import com.dynamsoft.cvr.CaptureVisionRouterException;
+import com.dynamsoft.cvr.CapturedResult;
+import com.dynamsoft.cvr.CapturedResultReceiver;
+import com.dynamsoft.dbr.BarcodeResultItem;
+import com.dynamsoft.dbr.DecodedBarcodesResult;
 import com.dynamsoft.dce.CameraEnhancer;
 import com.dynamsoft.dce.CameraEnhancerException;
 import com.dynamsoft.dce.CameraView;
 import com.dynamsoft.dce.utils.PermissionUtil;
+import com.dynamsoft.dlr.TextLineResultItem;
 import com.dynamsoft.license.LicenseManager;
 
 import java.io.BufferedReader;
@@ -31,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private CameraEnhancer mCamera;
     private CaptureVisionRouter mRouter;
     private AlertDialog mAlertDialog;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         }
         PermissionUtil.requestCameraPermission(this);
         CameraView cameraView = findViewById(R.id.camera_view);
+        textView = findViewById(R.id.textView);
         mCamera = new CameraEnhancer(cameraView, this);
         DSRect region = new DSRect(0,0.4f,1,0.6f,true);
         mRouter = new CaptureVisionRouter(this);
@@ -56,10 +67,39 @@ public class MainActivity extends AppCompatActivity {
             String template = readTemplate(R.raw.vin_template);
             mRouter.initSettings(template);
             mRouter.setInput(mCamera);
+            mRouter.addResultReceiver(new CapturedResultReceiver() {
+                @Override
+                public void onCapturedResultReceived(@NonNull CapturedResult result) {
+                    String barcode = "";
+                    String textLine = "";
+                    for (CapturedResultItem item:result.getItems()) {
+                        if (item.getType() == EnumCapturedResultItemType.CRIT_BARCODE) {
+                            barcode = ((BarcodeResultItem) item).getText();
+                        }
+                        if (item.getType() == EnumCapturedResultItemType.CRIT_TEXT_LINE) {
+                            textLine = ((TextLineResultItem) item).getText();
+                        }
+                    }
+                    displayResult(barcode,textLine);
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
         startScanning();
+    }
+
+    private void displayResult(String barcode,String textLine){
+        runOnUiThread(() -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Barcode Result: ");
+            sb.append(barcode);
+            sb.append("\n");
+            sb.append("Text Result: ");
+            sb.append(textLine);
+            sb.append("\n");
+            textView.setText(sb.toString());
+        });
     }
 
     private String readTemplate(int ID) throws IOException {
