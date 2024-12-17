a7233c2d9010de680d61e399a98c019e6499ffc4
xulihang <xulihanghai@163.com>
2024-12-17 13:55:52 +0800

    implement barcode reading

--- app/src/main/java/com/tonyxlh/vinscanner/MainActivity.java
+++ app/src/main/java/com/tonyxlh/vinscanner/MainActivity.java
@@ -1,15 +1,35 @@
 package com.tonyxlh.vinscanner;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.dynamsoft.core.basic_structures.CompletionListener;
+import com.dynamsoft.cvr.CaptureVisionRouter;
+import com.dynamsoft.cvr.CaptureVisionRouterException;
+import com.dynamsoft.dce.CameraEnhancer;
+import com.dynamsoft.dce.CameraEnhancerException;
+import com.dynamsoft.dce.CameraView;
+import com.dynamsoft.dce.utils.PermissionUtil;
+import com.dynamsoft.license.LicenseManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private CameraEnhancer mCamera;
+    private CaptureVisionRouter mRouter;
+    private AlertDialog mAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +40,66 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if (savedInstanceState == null) {
+            LicenseManager.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", this, (isSuccess, error) -> {
+                error.printStackTrace();
+            });
+        }
+        PermissionUtil.requestCameraPermission(this);
+        CameraView cameraView = findViewById(R.id.camera_view);
+        mCamera = new CameraEnhancer(cameraView, this);
+        mRouter = new CaptureVisionRouter(this);
+        try {
+            String template = readTemplate(R.raw.vin_barcode_template);
+            Log.d("DCV","template");
+            Log.d("DCV",template);
+            mRouter.initSettings(template);
+            mRouter.setInput(mCamera);
+        } catch (CaptureVisionRouterException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        startScanning();
+    }
+
+    private String readTemplate(int ID) throws IOException {
+        InputStream inp = this.getResources().openRawResource(ID);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inp));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        String content = out.toString();
+        reader.close();
+        return content;
+    }
+    private void startScanning(){
+        try {
+            mCamera.open();
+        } catch (CameraEnhancerException e) {
+            e.printStackTrace();
+        }
+        mRouter.startCapturing("ReadVINBarcode", new CompletionListener() {
+            @Override
+            public void onSuccess() {}
+            @Override
+            public void onFailure(int errorCode, String errorString) {
+                runOnUiThread(() -> showDialog("Error", String.format(Locale.getDefault(), "ErrorCode: %d %nErrorMessage: %s", errorCode, errorString)));
+            }
+        });
+    }
+
+    private void showDialog(String title, String message) {
+        if (mAlertDialog == null) {
+            mAlertDialog = new AlertDialog.Builder(this)
+                    .setCancelable(true)
+                    .setPositiveButton("OK", null)
+                    .create();
+        }
+        mAlertDialog.setTitle(title);
+        mAlertDialog.setMessage(message);
+        mAlertDialog.show();
     }
 }
